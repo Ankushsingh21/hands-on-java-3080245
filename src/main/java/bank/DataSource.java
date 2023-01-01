e3614554a1155284242e0cf97d8365f8b5e4adc4
=Ankushsingh21 <=singhankush9666@gmail.com>
2023-01-02 00:01:47 +0530

    DataSource file added

--- src/main/java/bank/DataSource.java
+++ src/main/java/bank/DataSource.java
@@ -1,11 +1,38 @@
 package bank;
 
+import java.lang.reflect.Method;
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class DataSource {
-  public static Connection connection(){
-    return null;
-    
+  public static Connection connect() {
+    String db_file = "jdbc:sqlite:resources/bank.db";
+    Connection connection = null;
+
+    try {
+      connection = DriverManager.getConnection(db_file);
+      System.out.println("we' are connected");
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+    return connection;
+
   }
-  
+
+  public static Customer getCustomer(String username) {
+    String sql = "select * from customer where username =?";
+
+    try (Connection connection = connect();
+        PreparedStatement statement = connection.prepareStatement(sql)) {
+      statement.setString(1, username);
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public static void main(String[] args) {
+    connect();
+  }
 }
