349a025d3eb569efa9017b593a3dfe1478f4eb87
Ankush Singh <43205706+Ankushsingh21@users.noreply.github.com>
2023-01-13 18:01:53 +0000

    datasoruce file edited

--- src/main/java/bank/DataSource.java
+++ src/main/java/bank/DataSource.java
@@ -1,9 +1,9 @@
 package bank;
 
-import java.lang.reflect.Method;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class DataSource {
@@ -22,17 +22,28 @@ public class DataSource {
   }
 
   public static Customer getCustomer(String username) {
-    String sql = "select * from customer where username =?";
-
+    String sql = "select * from customers where username = ?";
+    Customer customer = null;
     try (Connection connection = connect();
         PreparedStatement statement = connection.prepareStatement(sql)) {
       statement.setString(1, username);
+      try (ResultSet resultset = statement.executeQuery()) { // it will return a result set
+        customer = new Customer(
+            resultset.getInt("id"),
+            resultset.getString("name"),
+            resultset.getString("username"),
+            resultset.getString("password"),
+            resultset.getInt("account_id"));
+      }
+
     } catch (SQLException e) {
       e.printStackTrace();
     }
+    return customer;
   }
 
   public static void main(String[] args) {
-    connect();
+    Customer customer = getCustomer("twest8o@friendfeed.com");
+    System.out.println(customer.getName());
   }
 }
